0395f0cfe895e341f4e5474bd0c295e9f0e6833d
reability <4reability@gmail.com>
2022-07-15 04:18:03 +0300

    Initial

---
+++ Source/Routers/DreamRouter.py
@@ -0,0 +1,12 @@
+
+
+class AppRouter:
+    def __init__(self, bp):
+        self.bp = bp
+
+    def route_dreams(self, controller):
+        self.bp.route("/", methods=["GET"])(controller.get)
+        self.bp.route("/", methods=["POST"])(controller.store)
+        self.bp.route("/<int: dreamid>/job/<int: jobid>", methods=["POST"])(controller.get)
+        self.bp.route("/<int: dreamid>/job/<int: jobid>", methods=["UPDATE"])(controller.get)
+        self.bp.route("/<int: dreamid>/job/<int: jobid>", methods=["DELETE"])(controller.get)
